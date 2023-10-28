571c4b3c39b8b681134eda89a474fbbe24c651bf
MichaelSolati <mkslt04@gmail.com>
2023-10-28 09:45:03 -0700

    fix(Add to Home Screen): fix weird naming issue

--- src/components/examples/AddToHomeScreenButton.tsx
+++ src/components/examples/AddToHomeScreenButton.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 
-export default function AddToHomeScreen() {
+export default function AddToHomeScreenButton() {
   const [installPrompt, setInstallPrompt] = useState<any>();
 
   useEffect(() => {
