94b8d93a4d148d6379df749608c4eb2e97fd50f6
MichaelSolati <mkslt04@gmail.com>
2023-10-28 10:58:16 -0700

    feat(index): flesh out home page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from "astro/config";
-
 import react from "@astrojs/react";
 import serviceWorker from "astrojs-service-worker";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://fugu-demo.web.app",
-  integrations: [react(), serviceWorker()],
+  integrations: [react(), serviceWorker(), mdx()]
 });
